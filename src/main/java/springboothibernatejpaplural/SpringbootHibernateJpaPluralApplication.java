68fe92d96530094943f52265b60ae3e4a26ec4d4
OcampoPythonZen <eocampo1986@gmail.com>
2020-02-13 15:48:41 -0600

    First commit added

---
+++ src/main/java/springboothibernatejpaplural/SpringbootHibernateJpaPluralApplication.java
@@ -0,0 +1,13 @@
+package springboothibernatejpaplural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHibernateJpaPluralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHibernateJpaPluralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springboothibernatejpaplural/SpringbootHibernateJpaPluralApplicationTests.java
@@ -0,0 +1,13 @@
+package springboothibernatejpaplural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHibernateJpaPluralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
